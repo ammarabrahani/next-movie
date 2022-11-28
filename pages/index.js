e1677f7b71cb1b7d46fdd416e0b2827e8fe28b17
ammarabrahani <ammarabrahani@gmail.com>
2022-11-29 02:04:33 +0400

    change element

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-import { Box } from "@mui/material";
-
 export default function Home() {
-  return <Box>Create Next Movie APP</Box>;
+  return <div>Create Next Movie APP</div>;
 }
